411157eca0a6c89592ce719e1c3a96274c71f23b
Willams S. de Sousa <44249322+osdeving@users.noreply.github.com>
2021-01-13 16:59:15 -0300

    Update IdadeEmDias.cs

--- Resolvendo Algoritmos/IdadeEmDias.cs
+++ Resolvendo Algoritmos/IdadeEmDias.cs
@@ -2,30 +2,21 @@ using System;
 
 namespace Desafios.ResolvendoAlgoritmos
 {
-    class IdadeEmDias2IdadeEmAnosMesesDias
+    class IdadeEmDias
     {
         static void Main(string[] args)
         {
-            var idadeEmdias = ObtemIdadeEmDias();
-            
-            var anos = idadeEmDias / 365;
-            
-            var diasQueSobraram = idadeEmDias % 365;
+            var dias = int.Parse(Console.ReadLine());
+            var anos = dias / 365;
+            dias = dias % 365;
+            var meses = dias / 30;
+            dias = dias % 30;
 
-            var meses = diasQueSobraram / 30;
 
-            var dias = diasQueSobraram % 30;
-
-            MostraIdadeEmAnosMesesDias();
-        }
-
-        static int ObtemIdadeEmDias() => int.Parse(Console.ReadLine());
-        
-        static void MostraIdadeEmAnosMesesDias()
-        {
             Console.WriteLine($"{anos} ano(s)");
             Console.WriteLine($"{meses} mes(es)");
             Console.WriteLine($"{dias} dia(s)");
+
         }
     }
 }
